6534a44f1535a023a08f0d3c0805343ee40ad76c
lihui497748354 <30363199+lihui497748354@users.noreply.github.com>
2018-01-17 21:49:11 +0800

    springCloud

---
+++ eurekaServer/src/main/java/com/effort/eurekaserver/SpringcloudApplication.java
@@ -0,0 +1,14 @@
+package com.effort.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SpringcloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudApplication.class, args);
+	}
+}
---
+++ serviceFeign/src/main/java/com/servicefeign/interfaces/SchedualServiceHi.java
@@ -0,0 +1,16 @@
+package com.servicefeign.interfaces;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by Administrator on 2018/1/17.
+ * 定义一个feign接口，通过@ FeignClient（“服务名”），来指定调用哪个服务。比如在代码中调用了service-hi服务的“/hi”
+ */
+@FeignClient(value = "service-hi")
+public interface SchedualServiceHi {
+    @RequestMapping(value = "/hi",method = RequestMethod.GET)
+    String sayHiFromClientOne(@RequestParam(value = "name") String name);
+}
